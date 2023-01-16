d626d8232df18554fdbfaa81ff998269216f7780
Kristina Adigamova <kriwowak@yandex.ru>
2023-01-16 12:54:20 +0500

    Первый коммит

---
+++ java/com/example/bookofrecipes_3kurs/BookOfRecipes3kursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookofrecipes_3kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOfRecipes3kursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
